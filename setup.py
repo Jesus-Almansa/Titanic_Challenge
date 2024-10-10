02c90ac831f94ac1d0ef8159b8aee2b67cec5036
Jesus-Almansa <jtafernandez@gmail.com>
2024-10-10 09:01:03 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Titanic',
+    version='0.1.0',
+    packages=find_packages(),
+  author='Yisus',
+    description='Titanic challenge'
+)
+
